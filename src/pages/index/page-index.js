9615742590e60ff21525a9b9f900c27a11aa9862
SophieU <j.u.sophia@qq.com>
2018-04-12 09:43:45 +0800

    index page

--- src/pages/index/page-index.js
+++ src/pages/index/page-index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 class Index extends React.Component{
     render(){
-        console.log(window.location.pathname)
         return(
             <div>首页</div>
         )
